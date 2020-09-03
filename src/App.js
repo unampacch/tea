da4b37c9c3d09e55e97fc30322718caa4e23e888
Jonathan Bailon Segura <jonn59@gmail.com>
2020-09-02 21:41:39 -0500

    Correcion h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tutorial es estrategias de aprendizaje</h1>
+        <h1>Tutorial de estrategias de aprendizaje</h1>
       </header>
     </div>
   );
